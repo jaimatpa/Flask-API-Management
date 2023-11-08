411f9e2cf9d6fc14f4d5ba7385dca6001cf8053d
Amna <amna.syed@stonybrook.edu>
2023-11-08 04:35:13 +0000

    updated code

--- app_flasgger.py
+++ app_flasgger.py
@@ -15,6 +15,11 @@ def hello_get():
         type: string
         required: false
         default: World
+      - name: lastname
+        in: query
+        type: string
+        required: false
+        default: last name not provided
     responses:
       200:
         description: A greeting message
@@ -22,12 +27,15 @@ def hello_get():
     name = request.args.get('name', 'World')
     return f'Hello {name}!'
 
+    lastname = request.args.get('lastname', 'no last name provided')
+    return f'Hello {lastname}!'
+
     data = request.get_json()
     if data is None:
         return jsonify({'error': 'Invalid JSON'}), 400
     
     name = data.get('name', 'World')
-    return jsonify({'message': f'Hello {name}!'})
+    return jsonify({'message': f'Hello {name} {lastname}!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
